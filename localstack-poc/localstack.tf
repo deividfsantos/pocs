03f7d1ba0a7ec6d0d2ee5120693f7d2e908419e0
Deivid Santos <deividfranciscosantos@hotmail.com>
2023-09-01 10:44:41 -0300

    add kinesis

--- localstack-poc/localstack.tf
+++ localstack-poc/localstack.tf
@@ -8,6 +8,8 @@ provider "aws" {
 
   endpoints {
     s3 = "http://s3.localhost.localstack.cloud:4566"
+    ec2 = "http://localhost:4566"
+    kinesis  = "http://localhost:4566"
   }
 }
 
@@ -18,4 +20,38 @@ resource "aws_s3_bucket" "test-bucket" {
     Name        = "test-bucket"
     Environment = "Dev"
   }
+}
+
+data "aws_ami" "amazon_eks_node_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amazon-eks-node-linux"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon_eks_node_linux.id
+  instance_type = "t2.micro"
+ 
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_kinesis_stream" "kinesis_stream" {
+  name = "stream_publisher"
+  shard_count = 1
+  retention_period = 30
+
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes",
+  ]
 }
